c7972d00081efe0fd8f663f13610b0c844976e61
jvs <vonseg@protonmail.com>
2020-10-16 21:49:42 -0500

    Get the version from the init file.

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
+import re
 import setuptools
 
 
 def long_description():
-    try:
-        with open('README.md') as f:
-            return f.read()
-    except:
-        return ''
+    with open('README.md') as f:
+        return f.read()
 
 
 def install_requires():
@@ -14,9 +12,15 @@ def install_requires():
         return f.read().splitlines()
 
 
+def version():
+    with open('exemplary/__init__.py') as f:
+        regex = re.compile('\\n__version__\\s*=\\s*[\'"]([\\d\\.]+)[\'"]\n')
+        return regex.search(f.read()).group(1)
+
+
 setuptools.setup(
     name='exemplary',
-    version='0.0.1',
+    version=version(),
     author='jvs',
     author_email='vonseg@protonmail.com',
     url='https://github.com/jvs/exemplary',
